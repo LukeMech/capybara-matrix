7973a0367fc73960c50eaf46976955295e97ea31
LukeMech <lukaszjajkommz@gmail.com>
2024-01-18 16:33:41 +0100

    Try repairs

--- .github/workflows/scripts/genImg.py
+++ .github/workflows/scripts/genImg.py
@@ -1,17 +1,12 @@
 import sdkit
-from sdkit.models import download_models, resolve_downloaded_model_path, load_model
+from sdkit.models import download_model, resolve_downloaded_model_path, load_model
 from sdkit.generate import generate_images
 from sdkit.utils import log, save_images
 
 context = sdkit.Context()
 context.device = "cpu"
 
-download_models(
-
-    models={
-        "stable-diffusion": ["2.1-768-ema-pruned"],
-    }
-) # Downloads models
+download_model("stable-diffusion", "2.1-768-ema-pruned") # Downloads models
 
 context.model_paths["2.1-768-ema-pruned"] = resolve_downloaded_model_path("stable-diffusion", "2.1-768-ema-pruned")
 load_model(context, "2.1-768-ema-pruned")
