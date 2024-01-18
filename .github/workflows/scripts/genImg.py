c33aee45ce95e6258ff53ad1993cda83fd358960
LukeMech <lukaszjajkommz@gmail.com>
2024-01-18 16:22:33 +0100

    test

--- .github/workflows/scripts/genImg.py
+++ .github/workflows/scripts/genImg.py
@@ -1,14 +1,20 @@
 import sdkit
-from sdkit.models import download_model, resolve_downloaded_model_path, load_model
+from sdkit.models import download_models, resolve_downloaded_model_path, load_model
 from sdkit.generate import generate_images
 from sdkit.utils import log, save_images
 
-context = sdkit.Context(device="cpu")
+context = sdkit.Context()
+context.device = "cpu"
 
-download_model("stable-diffusion", "sd-xl-refiner-1.0") # Downloads models
+download_models(
 
-context.model_paths["sd-xl-refiner"] = resolve_downloaded_model_path("stable-diffusion", "sd-xl-refiner-1.0")
-load_model(context, "sd-xl-refiner")
+    models={
+        "stable-diffusion": ["2.1-768-ema-pruned"],
+    }
+) # Downloads models
+
+context.model_paths["2.1-768-ema-pruned"] = resolve_downloaded_model_path("stable-diffusion", "2.1-768-ema-pruned")
+load_model(context, "2.1-768-ema-pruned")
 
 images = generate_images(context, width=768, height=768, prompt="Very professional capybara photo", seed=42)
 save_images(images, dir_path="./tmpImages")
