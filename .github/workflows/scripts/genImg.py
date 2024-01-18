989dab683a0fdfc16793722fed28a471cbde7541
LukeMech <lukaszjajkommz@gmail.com>
2024-01-18 16:14:16 +0100

    hmhm

--- .github/workflows/scripts/genImg.py
+++ .github/workflows/scripts/genImg.py
@@ -1,16 +1,11 @@
 import sdkit
-from sdkit.models import download_models, resolve_downloaded_model_path, load_model
+from sdkit.models import download_model, resolve_downloaded_model_path, load_model
 from sdkit.generate import generate_images
 from sdkit.utils import log, save_images
 
 context = sdkit.Context(device="cpu")
 
-download_models(
-
-    models={
-        "stable-diffusion": ["sd-xl-refiner-1.0"],
-    }
-) # Downloads models
+download_model("stable-diffusion", "sd-xl-refiner-1.0") # Downloads models
 
 context.model_paths["sd-xl-refiner"] = resolve_downloaded_model_path("stable-diffusion", "sd-xl-refiner-1.0")
 load_model(context, "sd-xl-refiner")
