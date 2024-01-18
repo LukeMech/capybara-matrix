a04405645a768269142846e98dd4673a856d86e8
LukeMech <lukaszjajkommz@gmail.com>
2024-01-18 15:46:01 +0100

    Bugfix

--- genImg.py
+++ genImg.py
@@ -1,16 +1,16 @@
 import sdkit
 from sdkit.models import download_models, resolve_downloaded_model_path, load_model
 from sdkit.generate import generate_images
-from sdkit.utils import save_images
+from sdkit.utils import log, save_images
 
 context = sdkit.Context()
 
-download_models(model_type = 'stable-diffusion', model_id='2.1-768-ema-pruned') # downloads the known "SD 1.5-pruned-emaonly" model
+download_models(model_type="stable-diffusion", model_id="sd-xl-refiner-1.0") # Downloads model
 
-context.model_paths['stable-diffusion'] = resolve_downloaded_model_path('stable-diffusion', '2.1-768-ema-pruned')
-load_model(context, 'stable-diffusion')
+context.model_paths["stable-diffusion"] = resolve_downloaded_model_path("stable-diffusion", "sd-xl-refiner-1.0")
+load_model(context, "stable-diffusion")
 
-images = generate_images(context, width=768, height=768, prompt='Very professional capybara photo', seed=42)
-save_images(images, dir_path='./tmpImages')
+images = generate_images(context, width=768, height=768, prompt="Very professional capybara photo", seed=42)
+save_images(images, dir_path="./tmpImages")
 
 log.info("Generated images!")
