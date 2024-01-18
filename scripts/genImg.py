505d69533dc11fb10156b49ade057a8976b23edd
LukeMech <lukaszjajkommz@gmail.com>
2024-01-18 19:40:49 +0100

    YAYY

--- scripts/genImg.py
+++ scripts/genImg.py
@@ -20,7 +20,7 @@ context = sdkit.Context()
 context.device = "cpu"
 
 if len(sys.argv) != 2:
-    print("Json request file path missing")
+    print("Json prompts file path missing!")
     sys.exit(1)
 
 with open(sys.argv[1], 'r') as file:
