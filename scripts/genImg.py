ff1216cf06343952e9d856d27c9c217ba81708c5
LukeMech <lukaszjajkommz@gmail.com>
2024-01-18 19:47:40 +0100

    YAYYYYY

--- scripts/genImg.py
+++ scripts/genImg.py
@@ -46,7 +46,7 @@ log.info("Request: " + prompt)
 log.info("| Using:")
 log.info("Model type: " + properties["modelType"])
 log.info("Model name: " + properties["modelNames"][0])
-log.info("Inference count: " + properties["inference_count"])
+log.info("Inference count: " + str(properties["inference_count"]))
 
 images = generate_images(context, width=properties["x"], height=properties["y"], prompt=prompt, seed=42, num_inference_steps=properties["inference_count"])
 save_images(images, dir_path="./tmpImages")
