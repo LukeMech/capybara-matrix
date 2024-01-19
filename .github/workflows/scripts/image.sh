7a5b85614d0f8967a6684cd40434ec77226dc790
LukeMech <lukaszjajkommz@gmail.com>
2024-01-19 01:48:35 +0100

    Bugfix

--- .github/workflows/scripts/image.sh
+++ .github/workflows/scripts/image.sh
@@ -1,7 +1,7 @@
 pip install --upgrade pip
 pip install sdkit
 
-mkdir -p $HOME/tmp
+mkdir -p ./tmp
 python image.py $CONFIGPATH $MATRIXMODEL
 
-mv $HOME/tmp/*.jpeg $HOME/tmp/image/$(date '+%Y%m%d%H%M%S')-$JOBINDEX.jpeg
+mv ./tmp/*.jpeg ./tmp/image/$(date '+%Y%m%d%H%M%S')-$JOBINDEX.jpeg
