4950964086e9fa381c0b6379aa1b41928e8ecfef
LukaszMech <81306360+LukeMech@users.noreply.github.com>
2024-01-19 02:45:28 +0100

    Bypass for now

--- .github/workflows/scripts/prompt.sh
+++ .github/workflows/scripts/prompt.sh
@@ -1,5 +1,6 @@
-pip install --upgrade pip
-pip install rwkv torch numpy tqdm
+# pip install --upgrade pip
+# pip install rwkv torch numpy tqdm
 
-mkdir ./tmp
-python prompt.py $CONFIGPATH 
+# mkdir ./tmp
+# python prompt.py $CONFIGPATH
+echo "Nice capybara photo" > prompt.txt
