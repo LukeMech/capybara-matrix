b0377775f4eb42f7cf715b26f3d164b59c9ba171
LukeMech <lukaszjajkommz@gmail.com>
2024-01-19 21:23:53 +0100

    Well lessgo

--- scripts/prompt.sh
+++ scripts/prompt.sh
@@ -3,7 +3,7 @@ pip install -U g4f
 
 mkdir -p ./tmp
 if [ -n "$PROMPT" ]; then
-    python prompt.py $CONFIGPATH $PROMPT
+    python prompt.py $CONFIGPATH "$PROMPT"
 else 
     python prompt.py $CONFIGPATH
 fi
